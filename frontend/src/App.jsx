1fc9d356440b40b9b8bca5e04763518db945293c
= <=>
2024-11-05 13:04:28 +0200

    Repositioned the router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,49 @@
+import Login from "./Components/auth/Login"
+import Reg from "./Components/auth/Reg"
+import Friends from "./Components/Friends"
+import Home from "./Components/Home"
 import Navbar from "./Components/Navbar"
+import People from "./Components/People"
+import Post from "./Components/Post"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+
+const pageroutes = createBrowserRouter([
+  {
+    path:"/",
+    element: <> <Navbar/> <Home/> </>
+  },
+  {
+    path:"/post",
+    element: <> <Navbar/> <Post/> </>
+  },
+  {
+    path: "/friends",
+    element: <> <Navbar/> <Friends/> </>
+  },
+  {
+    path: "/people",
+    element: <> <Navbar/> <People/> </>
+  }
+])
+
+const authenticationRoutes = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/register",
+    element: <Reg/>
+  }
+])
 
 function App() {
   return(
-    <h2>hello</h2>
+    <>
+      <RouterProvider router={pageroutes} />
+      <RouterProvider router={authenticationRoutes} />
+    </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,35 +4,9 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Navbar from "./Components/Navbar.jsx";
-import Home from "./Components/Home.jsx";
-import Post from "./Components/Post.jsx";
-import Friends from "./Components/Friends.jsx";
-import People from "./Components/People.jsx";
-
-
-const pageroutes = createBrowserRouter([
-  {
-    path:"/",
-    element: <> <Navbar/> <Home/> </>
-  },
-  {
-    path:"/post",
-    element: <> <Navbar/> <Post/> </>
-  },
-  {
-    path: "/friends",
-    element: <> <Navbar/> <Friends/> </>
-  },
-  {
-    path: "/people",
-    element: <> <Navbar/> <People/> </>
-  }
-])
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={pageroutes} />
+    <App />
   </StrictMode>
 );
