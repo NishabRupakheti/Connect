48aed14e61420f18735674d181e6136c6c11f776
Nishab <nishabrupakheti2@gmail.com>
2024-11-28 17:49:39 +0200

    frontend : friends page added

--- frontend/src/pages/Friends.jsx
+++ frontend/src/pages/Friends.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+import { useAuth } from "../context/Context";
 
 const Friends = () => {
-  return (
-    <div>Friends</div>
-  )
-}
 
-export default Friends
+  const {token} = useAuth();
+
+  const [friends, setFriends] = useState([]);
+
+  const removeFriend = (index) => {
+    setFriends(friends.filter((_, i) => i !== index));
+  };
+
+  const getFriends = async ()=>{
+     const response = await axios.get("http://localhost:3000/friend/connection",{
+      headers:{
+        Authorization : `Bearer ${token}`
+      }
+     })
+    setFriends(response.data)
+
+  }
+
+  useEffect(()=>{
+    getFriends();
+  },[])
+
+  return (
+    <div className="container w-50 mt-4">
+      {friends.length === 0 ? (
+        <p>No friends to display</p>
+      ) : (
+        friends.map((friend, index) => (
+          <div key={index} className="card mb-3">
+            <div className="card-header">
+              <h5 className="card-title">{friend.userName}</h5>
+            </div>
+            <div className="card-body">
+              <p className="card-text">{friend.email}</p>
+              <button
+                onClick={() => removeFriend(index)}
+                className="btn btn-danger"
+              >
+                Remove friend
+              </button>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Friends;
